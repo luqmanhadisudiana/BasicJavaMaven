623b3ca1f66fec3e7bd25eb14b56ac4e3676b882
luqmanhadi <luqmanhadi800@gmail.com>
2022-06-19 22:19:44 +0700

    Update

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,10 +1,21 @@
+import src.main.java.Counter;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		Shouter shouter = new Shouter();
 		
-		shouter.shout("hurrah");
+		Counter counter = new Counter();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.increment();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
 	}
 
 }
